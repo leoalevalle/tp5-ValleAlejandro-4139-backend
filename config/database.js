7263bf8ff16a1f96873a7223a2ab241896901f62
leoalevalle2014 <leoalevalle2014@gmail.com>
2026-06-19 17:31:13 -0300

    se agrea el links.txt

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize'); 
  
 // Crea proyectodb en el servidory configura las credenciales de tu bd de PostgreSQL 
-const sequelize = new Sequelize('proyectodb', 'postgres', 'leonel2014', { 
+const sequelize = new Sequelize('proyectodb', 'postgres', 'tucontraseña', { 
   host: 'localhost', 
   dialect: 'postgres', 
   logging: false, // Evita que llene la consola con logs de consultas SQL básicas 
